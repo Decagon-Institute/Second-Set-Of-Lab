fe0fadbb31171cc2fe1f35eaa17a14a57b441320
DavidMogbeyi <davidm@l5lab.com>
2018-11-06 11:12:53 +0100

    Deleted the solutions

--- Evenly Spaced/evenlySpaced.js
+++
@@ -1,5 +0,0 @@
-function evenlySpaced(first, second, last) {
-    return (first - second == second - last) || (first - last == last - second) || (first - second == last - first);
-}
-
-module.exports = evenlySpaced;
